66502f7e0f05b67eb73ba75ea457d1398e327873
lyin <ling95613@163.com>
2018-01-04 16:48:21 +0800

    增加统计数据所用的常量

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,6 @@
 export const COST =  'COST';  //支出
 export const INCOME = 'INCOME'; //收入
 export const RECORDS_LIST = 'RECORDS_LIST'; //记录列表
+export const DAY_RECORDS = 'DAY_RECORDS'; //日记录
+export const WEEK_RECORDS = 'WEEK_RECORDS'; //周记录
+export const MONTH_RECORDS = 'MONTH_RECORDS'; //月记录
